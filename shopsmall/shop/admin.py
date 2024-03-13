2eb56651badd8837cc7d2530134209f237be037f
KaiCoder1 <131473153+KaiCoder1@users.noreply.github.com>
2024-03-12 18:23:36 -0700

    I setup some basic databases for the Customers, Bussieness, Products on the admin area.

--- shopsmall/shop/admin.py
+++ shopsmall/shop/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import testTable
+from .models import Product
+from .models import Bussiness
+from .models import Customers
 
 # Register your models here.
 admin.site.register(testTable)
+admin.site.register(Product)
+admin.site.register(Bussiness)
+admin.site.register(Customers)
--- shopsmall/shop/models.py
+++ shopsmall/shop/models.py
@@ -5,6 +5,34 @@ class testTable(models.Model):
     userName = models.CharField(max_length=20)
     password = models.CharField(max_length=64, unique=True)
 
+class Product(models.Model):
+    name = models.CharField("Product Name", max_length = 120)
+    creationDate = models.DateTimeField('Creation Date', blank = True)
+    businessID = models.IntegerField("Seller")
+    productID = models.IntegerField("Product ID")
 
 
+    def __str__(self):
+        return self.name
     
+class Customers(models.Model):
+        name = models.CharField('Customer Name', max_length = 100)
+        customerID = models.IntegerField('customer id')
+        email = models.EmailField('Email Address')
+        product = models.ForeignKey(Product, blank = True, null = True, on_delete= models.CASCADE)
+
+                
+        def __str__(self):
+                return self.name
+    
+class Bussiness(models.Model):
+    first_name = models.CharField(max_length = 120)
+    last_name = models.CharField(max_length = 120)
+    email = models.EmailField('business email')
+    products = models.ManyToManyField(Product, blank = True)
+
+    def __str__(self):
+         return self.email
+    
+
+        
