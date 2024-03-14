8f50c138e409471fc4c98f515e202be28b7c069f
Praveen Kandasamy <pravee.samy@gmail.com>
2024-03-13 17:22:31 -0700

    started login page still need to connect backend

--- shopsmall/shop/models.py
+++ shopsmall/shop/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class testTable(models.Model):
-    userName = models.CharField(max_length=20)
+    firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length = 200)
+    email = models.CharField(max_length = 200)
     password = models.CharField(max_length=64, unique=True)
+    date_created = models.DateTimeField(auto_now_add = True)
 
-
-
+    def _str_(self): 
+        return self.firstName + self.lastName
+    
+    
     
--- shopsmall/shop/views.py
+++ shopsmall/shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request, "shopComponents/home.html")
@@ -7,6 +8,19 @@ def home(request):
 def dashboard(request):
     return render(request, "shopComponents/dashboard.html")
 
-def navbar(request):
-    return render(request, "shopComponents/navbar.html")
-  
+
+def login_user(request): 
+    if request.method == "POST": 
+        pass 
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('shopComponenets/home.html')
+
+        else:
+            messages.success(request, ("Incorrect Username or Password"))
+            return render(request, 'shopComponents/home.html')
+    else: 
+        return render(request, 'members/login.html')
