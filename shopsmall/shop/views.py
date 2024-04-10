c9ea953234680f0e387a69d5eece3c179e9772f9
KaiCoder1 <131473153+KaiCoder1@users.noreply.github.com>
2024-04-10 11:51:27 -0700

    displaying info in front end

--- shopsmall/shop/views.py
+++ shopsmall/shop/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render,HttpResponse
 
+products = [
+    {
+        'seller': 'Karan',
+        'name': 'car',
+        'Price': '$100',
+        'date_posted': 'April 4th'
+    },
+    {
+        'seller': 'Ryan ',
+        'name': 'water bottle',
+        'Price': '$10',
+        'date_posted': 'April 5th'
+    }
+
+]
+
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "shop/home.html")
 
 # request
+
+def about(request):
+    context = {
+        'products': products
+    }
+    return render(request, "shop/product.html", context)
