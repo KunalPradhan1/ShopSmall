76035919c5a1f21e62211d37abb659bc4071c406
Praveen Kandasamy <pravee.samy@gmail.com>
2024-04-12 16:03:31 -0700

    register users done

--- shopsmall/shop/views.py
+++ shopsmall/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib import messages
 # Create your views here.
 def home(request):
@@ -8,6 +9,22 @@ def home(request):
 def dashboard(request):
     return render(request, "shopComponents/dashboard.html")
 
+def register_user(request): 
+    if request.method == "POST": 
+        username = request.POST['username']
+        firstName = request.POST['firstName']
+        lastName = request.POST['lastName']
+        email = request.POST['email']
+        password = request.POST['password']
+        rePassword = request.POST['rePassword']
+        myuser = User.objects.create_user(username, email, password)
+        myuser.firstName = firstName
+        myuser.lastName = lastName 
+        myuser.save()
+        messages.success(request, "Your account has been successfully created!")
+        return redirect('home')
+    return render(request, "members/register.html")
+    
 
 def login_user(request): 
     if request.method == "POST": 
