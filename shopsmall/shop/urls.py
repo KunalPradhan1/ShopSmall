c92430e2f5474b9da6286f7a3092adfe6641bff2
KaiCoder1 <131473153+KaiCoder1@users.noreply.github.com>
2024-04-23 17:39:32 -0700

    create product

--- shopsmall/shop/urls.py
+++ shopsmall/shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("product/", views.product, name = "product"),
-    path('base/', views.base, name = "base")
+    path('base/', views.base, name = "base"),
+    path('createproduct/', views.createProduct, name = "createProduct")
 ]
--- shopsmall/shop/views.py
+++ shopsmall/shop/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render,HttpResponse
 from .models import Product
 
-products = [
-    {
-        'seller': 'Karan',
-        'name': 'car',
-        'price': '$100',
-        'date_posted': 'April 4th',
-        'content': "an vehicle"
-    },
-    {
-        'seller': 'Ryan ',
-        'name': 'water bottle',
-        'price': '$10',
-        'date_posted': 'April 5th',
-        'content': "contians water"
+#Dummy Data 
+# products = [
+#     {
+#         'seller': 'Karan',
+#         'name': 'car',
+#         'price': '$100',
+#         'date_posted': 'April 4th',
+#         'content': "an vehicle"
+#     },
+#     {
+#         'seller': 'Ryan ',
+#         'name': 'water bottle',
+#         'price': '$10',
+#         'date_posted': 'April 5th',
+#         'content': "contians water"
 
-    }
+#     }
 
-]
+# ]
 
 # Create your views here.
 def home(request):
@@ -36,6 +37,19 @@ def product(request):
 
 def base(request):
     prod = {
-        'products': products
+        'products': Product.objects.all()
     }
     return render(request, "shop/base.html", prod)
+
+def createProduct(request):
+   
+    if request.method == 'POST':
+        seller1 = request.POST['seller']
+        name1 = request.POST['name']
+        price1 = request.POST['price']
+        content1 = request.POST['content']
+    
+    product = Product(name = name1, price = price1, content = content1, businessID = 2, productID = 1, seller = seller1)
+    product.save()
+    
+    return render(request, "shop/createproduct.html")
