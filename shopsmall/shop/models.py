ff67cb8973fd7a5b35674d35018060b28b4af62a
KunalPradhan1 <116305812+KunalPradhan1@users.noreply.github.com>
2024-04-29 14:25:07 -0700

    some of the front end done navbar is left
    finsih search and business and cart need to do navabar

--- shopsmall/shop/models.py
+++ shopsmall/shop/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 # Create your models here.
 class testTable(models.Model):
+<<<<<<< Updated upstream
     userName = models.CharField(max_length=20)
     password = models.CharField(max_length=64, unique=True)
 
 
 
+=======
+    firstName = models.CharField(max_length=200, default='SOME STRING')
+    lastName = models.CharField(max_length=200, default='SOME STRING')
+    email = models.CharField(max_length=200, default='SOME STRING')
+    password = models.CharField(max_length=64, unique=True, default='SOME STRING')
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.firstName + self.lastName
+    
+
+    
+>>>>>>> Stashed changes
     
--- shopsmall/shop/views.py
+++ shopsmall/shop/views.py
@@ -7,6 +7,33 @@ def home(request):
 def dashboard(request):
     return render(request, "shopComponents/dashboard.html")
 
+<<<<<<< Updated upstream
 def navbar(request):
     return render(request, "shopComponents/navbar.html")
   
+=======
+def business(request):
+    return render(request, "shopComponents/business.html")
+
+def cart(request):
+    return render(request, "members/cart.html")
+
+def search(request):
+    return render(request, "members/search.html")
+
+def login_user(request): 
+    if request.method == "POST": 
+        pass 
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('shopComponenets/home.html')
+
+        else:
+            messages.success(request, ("Incorrect Username or Password"))
+            return render(request, 'shopComponents/home.html')
+    else: 
+        return render(request, 'members/login.html')
+>>>>>>> Stashed changes
