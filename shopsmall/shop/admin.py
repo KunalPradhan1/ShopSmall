2a82bd6c3a316a7b452586b961fe8ecab98bd711
KaiCoder1 <131473153+KaiCoder1@users.noreply.github.com>
2024-05-01 18:37:12 -0700

    products

--- shopsmall/shop/admin.py
+++ shopsmall/shop/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from .models import testTable
+
 from .models import Product
-from .models import Bussiness
-from .models import Customers
 
 # Register your models here.
-admin.site.register(testTable)
 admin.site.register(Product)
-admin.site.register(Bussiness)
-admin.site.register(Customers)
+
--- shopsmall/shop/models.py
+++ shopsmall/shop/models.py
@@ -1,28 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class testTable(models.Model):
-    userName = models.CharField(max_length=20)
-    password = models.CharField(max_length=64, unique=True)
-
-class Customers(models.Model):
-        name = models.CharField('Customer Name', max_length = 100)
-        customerID = models.IntegerField('customer id')
-        email = models.EmailField('Email Address')
-        # product = models.ForeignKey(Product, blank = True, null = True, on_delete= models.CASCADE)
-
-                
-        def __str__(self):
-                return self.name
-    
-class Bussiness(models.Model):
-    first_name = models.CharField(max_length = 120)
-    last_name = models.CharField(max_length = 120)
-    email = models.EmailField('business email')
-    # products = models.ManyToManyField(Product, blank = True)
-
-    def __str__(self):
-         return self.email
 
 class Product(models.Model):
     name = models.CharField("Product Name", max_length = 120)
@@ -30,7 +8,7 @@ class Product(models.Model):
     date_posted = models.DateTimeField('Creation Date', auto_now_add = True)
     seller = models.CharField("Seller Name", max_length = 120, default = "ShopSmall")
     price = models.CharField("Price", max_length = 120, default= "$0")    
-    sellerB = models.ForeignKey(Bussiness, on_delete = models.CASCADE, null = True, blank = False)
+    # sellerB = models.ForeignKey(on_delete = models.CASCADE, null = True, blank = False)
     businessID = models.IntegerField("Seller")
     productID = models.IntegerField("Product ID")
     productImage = models.ImageField(null = True, blank = True, upload_to="images/")
