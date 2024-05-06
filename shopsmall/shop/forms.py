87a2f69b1191357441b71a555eb30ec28528e9ae
KunalPradhan1 <116305812+KunalPradhan1@users.noreply.github.com>
2024-05-06 13:41:26 -0700

    login and half of registration of front end
    this has some of the front end done

--- shopsmall/shop/forms.py
+++ shopsmall/shop/forms.py
@@ -5,16 +5,26 @@ from django.core.exceptions import ValidationError
 
 class LoginForm(forms.Form):
     username = forms.CharField(
+        label = "",
         widget= forms.TextInput(
             attrs={
+<<<<<<< Updated upstream
                 "class": "form-control my-custom-class"
+=======
+                "class": "form-control username-input",
+                "placeholder": "Username",
+                "style": "height: 50px;"
+>>>>>>> Stashed changes
             }
-        )
+        ) 
     )
     password = forms.CharField(
+        label = "",
         widget=forms.PasswordInput(
             attrs={
-                "class": "form-control"
+                "class": "form-control password-input",
+                "placeholder": "Password",
+                "style": "margin-top: 15px; height: 50px;"
             }
         )
     )
@@ -22,33 +32,92 @@ class LoginForm(forms.Form):
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
+        label = "",
         widget=forms.TextInput(
             attrs={
-                "class": "form-control"
+                "class": "form-control",
+                "style": "height: 50px;",
+                "placeholder": "Username"
             }
         )
     )
     password1 = forms.CharField(
+        label = "",
         widget=forms.PasswordInput(
             attrs={
-                "class": "form-control"
+                "class": "form-control",
+                "style": "margin-top: 15px; height: 50px;",
+                "placeholder": "Password"
+                
             }
         )
     )
     password2 = forms.CharField(
+        label = "",
         widget=forms.PasswordInput(
             attrs={
-                "class": "form-control"
+                "class": "form-control",
+                "style": "margin-top: 15px; height: 50px;",
+                "placeholder": "Re-enter Password"
             }
         )
     )
     email = forms.CharField(
+        label = "",
         widget=forms.TextInput(
             attrs={
-                "class": "form-control"
+                "class": "form-control",
+                "style": "margin-top: 15px; height: 50px;",
+                "placeholder": "Email"
             }
         )
     )
+
+    is_business = forms.BooleanField(
+    label = "Business",
+    widget=forms.CheckboxInput(
+        attrs={
+             "style": "transform: scale(1.25);",
+             
+            # Add more attributes here...
+        }
+    ),
+    required=False,  # Set to False if you want to make this field optional
+)
+    
+    is_customer = forms.BooleanField(
+    label = "Customer",
+    widget=forms.CheckboxInput(
+        attrs={
+             "style": "transform: scale(1.25);",
+            # Add more attributes here...
+        }
+    ),
+    required=False,  # Set to False if you want to make this field optional
+)
+    
+    phone_number = forms.IntegerField(
+    widget=forms.NumberInput(
+        attrs={
+            "class": "form-control",
+            "placeholder": "1231234567"
+            # Add more attributes here...
+
+        }
+    ),
+    required=True,  # Set to False if you want to make this field optional
+)
+
+    location = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control" 
+                ,
+                "placeholder": "123 Main St, San Jose, CA, 95116"
+            }
+        )
+    )
+    
 
     class Meta:
         model = User
--- shopsmall/shop/views.py
+++ shopsmall/shop/views.py
@@ -7,12 +7,69 @@ def home(request):
 def dashboard(request):
     return render(request, "shopComponents/dashboard.html")
 
+<<<<<<< Updated upstream
+=======
+@login_required(login_url = "login")
+def businessDashboard(request): 
+    if getattr(request.user, 'is_business', False):
+        user_products = Product.objects.filter(businessID = request.user.id)
+        context = {
+        'title': 'product',
+        'products': user_products
+         }
+        return render(request, "shopComponents/businessDashboard.html", context)
+    else:
+        return redirect("login")
+
+
+def register(request):
+    msg = None
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, label_suffix='')
+        if form.is_valid():
+            user = form.save()
+            msg = 'user created'
+            return redirect('login')
+        else:
+            msg = 'form is not valid'
+    else:
+        form = SignUpForm()
+    return render(request,'shopComponents/register.html', {'form': form, 'msg': msg})
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None, label_suffix='')
+    msg = None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_business:
+                login(request, user)
+                return redirect('bDashboard')
+            elif user is not None and user.is_customer:
+                login(request, user)
+                return redirect('cDashboard')
+            else:
+                msg= 'invalid credentials'
+        else:
+            msg = 'error validating form'
+    return render(request, 'shopComponents/login.html', {'form': form, 'msg': msg})
+
+
+
+def logout(request): 
+    auth.logout(request)
+    return redirect("home")
+>>>>>>> Stashed changes
 def business(request):
     return render(request, "shopComponents/business.html")
 
 def cart(request):
     return render(request, "members/cart.html")
 
+<<<<<<< Updated upstream
 #def search(request):
   #  search_term = request.GET.get('search', '')
   #  search_form = request.GET.get('SearchForm', '')
@@ -36,6 +93,15 @@ def login_user(request):
 
 
 
+=======
+# def search(request):
+#     search_term = request.GET.get('search', '')
+#     search_form = request.GET.get('SearchForm', '')
+#     return render(request, "members/search.html",{ 'search_term': search_term, 'search_form': search_form }) 
+
+
+@login_required(login_url = "login")
+>>>>>>> Stashed changes
 def search(request):
     if request.method == "POST":
         searched = request.POST['searched']
